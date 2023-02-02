4b557537be0f61b4c5f6dc927498d52f401e830b
Sofia <sofiaxrk@gmail.com>
2023-02-02 17:19:58 +0100

    classes added

---
+++ classes.js
@@ -0,0 +1,14 @@
+class Rectangle {
+    // is used to Setup your object
+    constructor () {
+        console.log("The Rectangle is being created!");
+
+        this.width = 5;
+        this.height = 3;
+        this.color = "blue"; //instance properties 
+    }
+} 
+
+
+let myRectangle1 = new Rectangle();
+let myRectangle2 = new Rectangle();
